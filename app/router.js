811e625bb3cbcbf59dac5752441a9778587d3607
Theresa Tan <thetan@thetan-mn1.linkedin.biz>
2021-06-29 12:10:16 -0700

    building pages

--- app/router.js
+++ app/router.js
@@ -6,4 +6,8 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('about');
+  // allows for shorter route and template name | getting-in-touch is the url
+  this.route('contact', {path: '/getting-in-touch'});
+});
